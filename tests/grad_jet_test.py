99c1b426877e3da61d03cf97ee3cbcea4c202257
jessebett <jessebett@gmail.com>
2019-11-26 12:16:41 -0500

    Working gradients through jet of ode solution!

--- tests/grad_jet_test.py
+++ tests/grad_jet_test.py
@@ -87,3 +87,80 @@ def test_grad_mlp():
   assert np.allclose(d3fdm_jet,d3fdm_jvp)
   assert np.allclose(d5fdm_jet,d5fdm_jvp)
 
+#TODO: where to put this?
+def sol_recursive(f,z,t):
+  # closure to expand z
+  def g(z):
+    return f(z,t)
+
+  # First coeffs computed recursively
+  (y0, [y1h]) = jet(g, (z, ), ((np.ones_like(z), ), ))
+  (y0, [y1, y2h]) = jet(g, (z, ), (( y0, y1h,), ))
+  (y0, [y1, y2, y3h]) = jet(g, (z, ), ((y0, y1, y2h), ))
+  (y0, [y1, y2, y3, y4h]) = jet(g, (z, ), ((y0, y1, y2, y3h), ))
+  (y0, [y1, y2, y3, y4, y5h]) = jet(g, (z, ),
+                                    ((y0, y1, y2, y3, y4h), ))
+  (y0, [y1, y2, y3, y4, y5,y6h]) = jet(g, (z, ),
+                                    ((y0, y1, y2, y3, y4, y5h), ))
+
+  
+  return (y0, [y1, y2, y3, y4, y5])
+
+def test_grad_sol_mlp():
+  sigm = lambda x: 1. / (1. + np.exp(-x))
+  def mlp(M1,M2,x):
+    return np.dot(sigm(np.dot(x,M1)),M2)
+  f_mlp = lambda x: mlp(M1,M2,x)
+
+  M1,M2 = (npr.randn(10,10), npr.randn(10,10))
+  x= npr.randn(2,10)
+
+  def sol_M1(M1):
+    # wrapper for expected arguments of sol
+    f_dynamics = lambda x,t : mlp(M1,M2,x)
+    return sol_recursive(f_dynamics,x,1.)
+
+  # print(sol_M1(M1))
+  # print((lambda m1: np.sum(sol_M1(m1)[0]))(M1))
+  print(grad(lambda m1: np.sum(sol_M1(m1)[1][4]))(M1))
+
+
+  # terms_in = [np.ones_like(x)]+ [np.zeros_like(x)]*5
+  # jvp_test_jet(f_mlp,(x,),[terms_in])
+
+
+  # def jvp_mlp(M1):
+  #   return jvp_taylor(lambda x: mlp(M1,M2,x), (x,), (terms_in,))
+  #
+  # def jet_mlp(M1):
+  #   return jet(lambda x: mlp(M1,M2,x), (x,), (terms_in,))
+
+
+  # grad through primal of jvp is the same as grad through primal
+  # assert np.allclose(grad(lambda m1 : jvp_mlp(m1)[0])(M1),grad(lambda m1 : mlp(m1,M2,x))(M1))
+
+  # grad through primal of jet is the same as grad through primal
+  # BROKE! :(
+  # assert np.allclose(grad(lambda m1 : jet_mlp(m1)[0])(M1),grad(lambda m1 : mlp(m1,M2,x))(M1))
+
+  # Jet and JVP for mlp are same
+  # assert np.allclose(jet_mlp(M1)[1],jvp_mlp(M1)[1])
+
+  # Meat and Potatoes of it all:
+  # gradient of second x-derivative wrt m1 with jvp
+  # d2fdm_jvp = grad(lambda m1 : jvp_mlp(m1)[1][0])(M1)
+  # gradient of third x-derivative wrt m1 with jvp
+  # d3fdm_jvp = grad(lambda m1 : jvp_mlp(m1)[1][1])(M1)
+  # gradient of fifth x-derivative wrt m1 with jvp
+  # d5fdm_jvp = grad(lambda m1 : jvp_mlp(m1)[1][4])(M1)
+
+  # gradient of second x-derivative wrt m1 with jet
+  # d2fdm_jet = grad(lambda m1 : jet_mlp(m1)[1][0])(M1)
+  # gradient of third x-derivative wrt m1 with jet
+  # d3fdm_jet = grad(lambda m1 : jet_mlp(m1)[1][1])(M1)
+  # gradient of fifth x-derivative wrt m1 with jet
+  # d5fdm_jet = grad(lambda m1 : jet_mlp(m1)[1][4])(M1)
+
+  # assert np.allclose(d2fdm_jet,d2fdm_jvp)
+  # assert np.allclose(d3fdm_jet,d3fdm_jvp)
+  # assert np.allclose(d5fdm_jet,d5fdm_jvp)
