c5a0b0d75edc66491295faa7e429edce05f3c827
Jacob Kelly <jacob.jin.kelly@gmail.com>
2019-11-28 13:01:12 -0500

    partial refactor to run test

--- examples/nodes_1d_toy.py
+++ examples/nodes_1d_toy.py
@@ -42,8 +42,6 @@ parse_args = parser.parse_args()
 
 config.update('jax_enable_x64', True)
 
-
-### TOY DATA ###
 D = 1
 true_y0_range = 3
 true_fn = lambda x: x ** 3
@@ -54,6 +52,7 @@ true_y = np.concatenate((np.expand_dims(true_y0, axis=0),
                         np.expand_dims(true_y1, axis=0)),
                         axis=0)  # (T, N, D)
 t = np.array([0., 1.])  # (T)
+
 
 @jax.jit
 def get_batch(key):
@@ -71,6 +70,7 @@ def get_batch(key):
                              axis=0)                                                    # (T, M, D)
     return key, batch_y0, batch_t, batch_y
 
+
 @jax.jit
 def pack_batch(key):
     """
@@ -78,28 +78,30 @@ def pack_batch(key):
     """
     key, batch_y0, batch_t, batch_y = get_batch(key)
 
-    batch_y0, batch_y1 = batch_y
-    # batch_y0_t = np.concatenate((batch_y0,np.expand_dims(np.repeat(batch_t[0],parse_args.batch_size),axis=1)),axis=1)
-    # batch_y1_t = np.concatenate((batch_y1,np.expand_dims(np.repeat(batch_t[1],parse_args.batch_size),axis=1)),axis=1)
-    #
-    # batch_y_t = np.array((batch_y0_t, batch_y1_t)) #(T, BS, D+1)
-    return key, batch_y #(T, BS, D)
+    return key, batch_y     #(T, BS, D)
 
-def append_time(y,t):
+
+def append_time(y, t):
   """
   y::(BS,D)
   t::scalar
   yt::(BS,D+1)
   """
-  yt = np.concatenate((y,np.expand_dims(np.repeat(t, parse_args.batch_size),axis=1)),axis=1)
+  yt = np.concatenate((y, np.expand_dims(np.repeat(t, parse_args.batch_size), axis=1)), axis=1)
   return yt
 
 
-### MODEL ###
 def sigmoid(z):
-    return 1./(1. + np.exp(-z))
+  """
+  Defined used np prims.
+  """
+  return 1./(1. + np.exp(-z))
+
 
 def predict(params, y_t):
+  """
+  MLP for dynamics of ODE.
+  """
   w_1, b_1 = params[0]
   z_1 = np.dot(y_t, w_1) + np.expand_dims(b_1, axis=0)
   out_1 = sigmoid(z_1)
@@ -109,20 +111,27 @@ def predict(params, y_t):
 
   return z_1
 
+
 def jet_wrap_predict(params):
   """
   Function which returns a closure that has the correct API for jet.
   """
-  return lambda z, t: predict(params, append_time(z,t))
+  return lambda z, t: predict(params, append_time(z, t))
 
-def sol_recursive(f,z,t):
-# closure to expand z
+
+def sol_recursive(f, z, t):
+  """
+  Recursively compute higher order derivatives of dynamics of ODE.
+  """
   def g(z):
-      return f(z,t)
+    """
+    Closure to expand z.
+    """
+    return f(z, t)
 
   # First coeffs computed recursively
   (y0, [y1h]) = jet(g, (z, ), ((np.ones_like(z), ), ))
-  (y0, [y1, y2h]) = jet(g, (z, ), (( y0, y1h,), ))
+  (y0, [y1, y2h]) = jet(g, (z, ), ((y0, y1h,), ))
   (y0, [y1, y2, y3h]) = jet(g, (z, ), ((y0, y1, y2h), ))
   # (y0, [y1, y2, y3, y4h]) = jet(g, (z, ), ((y0, y1, y2, y3h), ))
   # (y0, [y1, y2, y3, y4, y5h]) = jet(g, (z, ),
@@ -132,21 +141,20 @@ def sol_recursive(f,z,t):
   #
 
   # return (y0, [y1, y2, y3, y4, y5])
-  return  (y0, [y1, y2])
+  return (y0, [y1, y2])
 
 # Neural ODE
 @jax.jit
-def dynamics(y,t,*args):
+def dynamics(y, t, *args):
   """
   y::unravel((BS,D))
   t::scalar
   args::flat_params
   """
-  y = ravel_y(y) # because jax needs flat y
-  yt = append_time(y,t)
-  dydt = predict(ravel_params(np.array(args)),yt)
-  return np.ravel(dydt) # jax needs flat y
-
+  y = ravel_y(y)  # because jax needs flat y
+  yt = append_time(y, t)
+  dydt = predict(ravel_params(np.array(args)), yt)
+  return np.ravel(dydt)  # jax needs flat y
 
 
 @jax.jit
@@ -156,26 +164,21 @@ def loss_fun(pred, target):
     """
     return np.mean((pred - target) ** 2)
 
+
 @jax.jit
 def nodes_predict_and_loss(true_ys, y0, ts, flat_params):
     """
     Return function loss(params) for gradients
     """
-    # true_ys, odeint_args = args[0], args[1:]
-    ys_pred = nodeint(y0,ts,*flat_params)
+    ys_pred = nodeint(y0, ts, *flat_params)
     ys_pred = ravel_y(ys_pred[-1])
-    return loss_fun(true_ys,ys_pred)
+    return loss_fun(true_ys, ys_pred)
 
-# @jax.jit
-# def nodes_loss(b_y1,b_y0,params):
-#    # = args
-#   return nodes_predict(b_y1,np.ravel(b_y0),np.array([0.,1.]), params)
 
 # Main:
 rng = random.PRNGKey(0)
 t0 = 0.
-# ts = np.linspace(0.,1.,100)
-ts = np.array([0.,1.])
+ts = np.array([0., 1.])
 
 # initialize the parameters
 hidden_dim = 50
@@ -199,80 +202,64 @@ opt_state = opt_init(flat_params)
 
 # ODE Integrator
 nodeint = build_odeint(dynamics)
-grad_nodes_loss = jax.jit(grad(nodes_predict_and_loss,argnums=-1))
+grad_nodes_loss = jax.jit(grad(nodes_predict_and_loss, argnums=-1))
 
 assert parse_args.data_size % parse_args.batch_size == 0
 batch_per_epoch = parse_args.data_size // parse_args.batch_size
 
-itr = 0
-for epoch in range(parse_args.nepochs):
-  key = rng
-  for batch in range(batch_per_epoch):
-    itr+=1
-    print("Iteration:",itr)
-    key, batch_y = pack_batch(key)
-    b_y0, b_y1 = batch_y
 
-    # y_sol = nodeint(np.ravel(b_y0),ts,*flat_params) #(T,BS*D)
-    # y_sol = np.array(jax.map(ravel_y,y_sol)) # (T,BS,D)
-    # b_y1_pred = y_sol[-1]
-    # loss_fun(b_y1_pred,b_y1)
-
-    flat_params = get_params(opt_state)
-    dldp = grad_nodes_loss(b_y1,np.ravel(b_y0), ts, flat_params)
-    opt_state = opt_update(itr,dldp,opt_state)
-
-    # print loss:
-    if True:
-      y_sol = nodeint(np.ravel(b_y0),ts,*flat_params) #(T,BS*D)
-      y_sol = np.array(jax.map(ravel_y,y_sol)) # (T,BS,D)
-      b_y1_pred = y_sol[-1]
-      print(loss_fun(b_y1_pred,b_y1))
-
-### Integrate regularizer
-def append_aug(y,r):
+def append_aug(y, r):
   """
   y::(BS,D)
   r::(BS,R)
   yr::(BS,D+R)
   """
-  yr = np.concatenate((y,r),axis=1)
+  yr = np.concatenate((y, r), axis=1)
   return yr
 
+
 def unpack_aug(yr):
-  return yr[:,:D],yr[:,D:]
+  return yr[:, :D], yr[:, D:]
+
 
 @jax.jit
-def r2(y,t,params):
-  return np.sum(sol_recursive(jet_wrap_predict(ravel_params(params)),y,t)[1][0]**2,axis=1,keepdims=True)
+def r2(y, t, params):
+  return np.sum(sol_recursive(jet_wrap_predict(ravel_params(params)), y, t)[1][0]**2, axis=1, keepdims=True)
+
 
 @jax.jit
-def aug_dynamics(yr,t,*args):
+def aug_dynamics(yr, t, *args):
   """
   yr::unravel((BS,D+R))
   t::scalar
   args::flat_params
   return::unravel((BS,D+R))
   """
-  y,r = ravel_yr(yr)[:,:D], ravel_yr(yr)[:,D:] # because jax needs flat yr
-  yt = append_time(y,t)
-  dydt = predict(ravel_params(np.array(args)),yt)
-  drdt = r2(y,t,np.array(args))
+  y, r = ravel_yr(yr)[:, :D], ravel_yr(yr)[:, D:]   # because jax needs flat yr
+  yt = append_time(y, t)
+  dydt = predict(ravel_params(np.array(args)), yt)
+  drdt = r2(y, t, np.array(args))
 
-  return np.ravel(append_aug(dydt,drdt)) # jax needs flat y
+  return np.ravel(append_aug(dydt, drdt))            # jax needs flat y
+
 
 nodeint_aug = jax.jit(build_odeint(aug_dynamics))
 
-def aug_init(y):
-  return append_aug(y,np.zeros_like(y))
 
+def aug_init(y):
+  return append_aug(y, np.zeros_like(y))
+
+
+rng, batch_y = pack_batch(rng)
+b_y0, b_y1 = batch_y
 flat_yr, ravel_yr = ravel_pytree(aug_init(b_y0))
+
 
 @jax.jit
 def loss_aug(y0, ts, params):
   # Integrate Augmented Dynamics
-  yr_sol = nodeint_aug(np.ravel(aug_init(y0)),ts,*params)
-  yr_sol = np.array(jax.map(ravel_yr,yr_sol))
+  yr_sol = nodeint_aug(np.ravel(aug_init(y0)), ts, *params)
+  yr_sol = np.array(jax.map(ravel_yr, yr_sol))
 
   # Get final state
   yr1 = yr_sol[-1]
@@ -283,32 +270,65 @@ def loss_aug(y0, ts, params):
   # compute mean
   return np.mean(r_pred)
 
-loss_aug(b_y0,ts,flat_params) 
 
-dr2dp_func=jax.jit(grad(loss_aug, argnums=-1))
+def run(reg, lam, rng, dirname):
+    """
+    Run the Neural ODEs method.
+    """
+    print("Reg: %s\tLambda %.4e" % (reg, lam))
+    print("Reg: %s\tLambda %.4e" % (reg, lam), file=sys.stderr)
 
-dr2dp = dr2dp_func(b_y0,ts,flat_params)
+    itr = 0
+    for epoch in range(parse_args.nepochs):
+      for batch in range(batch_per_epoch):
+        itr += 1
+        print("Iteration:", itr)
+        rng, batch_y = pack_batch(rng)
+        b_y0, b_y1 = batch_y
+
+        flat_params = get_params(opt_state)
+        dldp = grad_nodes_loss(b_y1, np.ravel(b_y0), ts, flat_params)
+        opt_state = opt_update(itr, dldp, opt_state)
+
+        # print loss:
+        if True:
+          y_sol = nodeint(np.ravel(b_y0), ts, *flat_params)     # (T, BS*D)
+          y_sol = np.array(jax.map(ravel_y, y_sol))             # (T, BS, D)
+          b_y1_pred = y_sol[-1]
+          print(loss_fun(b_y1_pred, b_y1))
 
 
-### For testing gradients 
+if __name__ == "__main__":
+  test = True
+  if test:
+    import numpy as onp
 
-# numerical grad test
-import numpy as onp
-def nd(f, x, eps=1e-5):
-  flat_x, unravel = ravel_pytree(x)
-  dim = len(flat_x)
-  g = onp.zeros_like(flat_x)
-  for i in range(dim):
-    d = onp.zeros_like(flat_x)
-    d[i] = eps
-    g[i] = (f(unravel(flat_x + d)) - f(unravel(flat_x - d))) / (2.0 * eps)
-  return g
+    def nd(f, x, eps=1e-5):
+      """
+      Numerical differentiation
+      """
+      flat_x, unravel = ravel_pytree(x)
+      dim = len(flat_x)
+      g = onp.zeros_like(flat_x)
+      for i in range(dim):
+        d = onp.zeros_like(flat_x)
+        d[i] = eps
+        g[i] = (f(unravel(flat_x + d)) - f(unravel(flat_x - d))) / (2.0 * eps)
+      return g
 
+    # test grad ode jet sol nn
+    dr2dp_func = jax.jit(grad(loss_aug, argnums=-1))
+    dr2dp = dr2dp_func(b_y0, ts, flat_params)
+    # make closure for numerical version
+    dr2_p_closure = jax.jit(lambda params: loss_aug(b_y0, ts, params))
+    dr2dp_numerical = nd(dr2_p_closure, flat_params)
 
-# test grad ode jet sol nn
-dr2dp_func=jax.jit(grad(loss_aug, argnums=-1))
-dr2dp = dr2dp_func(b_y0,ts,flat_params)
+    tmp = onp.abs(dr2dp - dr2dp_numerical)
+    print("max abs. diff", onp.max(tmp))
+    print("mean abs. diff", onp.mean(tmp))
+    print("median abs. diff", onp.median(tmp))
+    print(tmp)
+    assert onp.allclose(dr2dp, dr2dp_numerical, atol=1e-5)
 
-# make closure for numerical version
-dr2_p_closure = jax.jit(lambda params : loss_aug(b_y0,ts,params))
-dr2dp_numerical = nd(dr2_p_closure, flat_params)
+  # assert os.path.exists(parse_args.dirname)
+  # run(parse_args.reg, parse_args.lam, random.PRNGKey(0), parse_args.dirname)
