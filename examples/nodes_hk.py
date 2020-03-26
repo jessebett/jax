2d0926aa00f1fc396a1bd7e2fb24bfe14a539097
Jacob Kelly <jacob.jin.kelly@gmail.com>
2020-03-26 18:02:37 -0400

    add lr schedule

--- examples/nodes_hk.py
+++ examples/nodes_hk.py
@@ -11,6 +11,7 @@ import haiku as hk
 import tensorflow_datasets as tfds
 
 import jax
+from jax import lax
 import jax.numpy as jnp
 from jax.experimental import optimizers
 from jax.experimental.ode import build_odeint, odeint
@@ -486,7 +487,14 @@ def run():
     forward, model = init_model()
     grad_fn = jax.grad(lambda *args: loss_fn(forward, *args))
 
-    opt_init, opt_update, get_params = optimizers.adam(step_size=parse_args.lr)
+    def lr_schedule(train_itr):
+        _epoch = train_itr // num_batches
+        id = lambda x: x
+        return lax.cond(_epoch < 60, 1e-1, id, 0,
+                        lambda _: lax.cond(_epoch < 100, 1e-2, id, 0,
+                                           lambda _: lax.cond(_epoch < 140, 1e-3, id, 1e-4, id)))
+
+    opt_init, opt_update, get_params = optimizers.momentum(step_size=lr_schedule, mass=0.9)
     opt_state = opt_init(model["params"])
 
     @jax.jit
