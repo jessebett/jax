e90097ab54818f12a404bbd2513ee89e484b975c
Jacob Kelly <jacob.jin.kelly@gmail.com>
2020-05-10 10:13:10 -0400

    physio r4

--- examples/nodes_physionet.py
+++ examples/nodes_physionet.py
@@ -37,7 +37,7 @@ parser.add_argument('--rtol', type=float, default=1.4e-8)
 parser.add_argument('--method', type=str, default="dopri5")
 parser.add_argument('--no_vmap', action="store_true")
 parser.add_argument('--init_step', type=float, default=1.)
-parser.add_argument('--reg', type=str, choices=['none', 'r2', 'r3'], default='none')
+parser.add_argument('--reg', type=str, choices=['none', 'r2', 'r3', 'r4'], default='none')
 parser.add_argument('--test_freq', type=int, default=640)
 parser.add_argument('--save_freq', type=int, default=640)
 parser.add_argument('--dirname', type=str, default='tmp')
@@ -107,11 +107,11 @@ def sol_recursive(f, z, t):
   (y0, [y1h]) = jet(g, (z_t, ), ((jnp.ones_like(z_t), ), ))
   (y0, [y1, y2h]) = jet(g, (z_t, ), ((y0, y1h,), ))
   (y0, [y1, y2, y3h]) = jet(g, (z_t, ), ((y0, y1, y2h), ))
-  # (y0, [y1, y2, y3, y4h]) = jet(g, (z_t, ), ((y0, y1, y2, y3h), ))
+  (y0, [y1, y2, y3, y4h]) = jet(g, (z_t, ), ((y0, y1, y2, y3h), ))
 
   return (jnp.reshape(y0[:-1], z_shape), [jnp.reshape(y1[:-1], z_shape),
-                                          jnp.reshape(y2[:-1], z_shape)])
-                                          # jnp.reshape(y3[:-1], z_shape)])
+                                          jnp.reshape(y2[:-1], z_shape),
+                                          jnp.reshape(y3[:-1], z_shape)])
 
 
 class LatentGRU(hk.Module):
