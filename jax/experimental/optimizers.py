0413a0ef74c3b0d45fe54dca1c1d71773df8cc6d
Jacob Kelly <jacob.jin.kelly@gmail.com>
2020-05-21 11:31:53 -0400

    actually fix merge in opt

--- jax/experimental/optimizers.py
+++ jax/experimental/optimizers.py
@@ -383,7 +383,6 @@ def adam(step_size, b1=0.9, b2=0.999, eps=1e-8):
   return init, update, get_params
 
 
-
 @optimizer
 def adamax(step_size, b1=0.9, b2=0.999, eps=1e-8):
   """Construct optimizer triple for AdaMax (a variant of Adam based on infinity norm).
