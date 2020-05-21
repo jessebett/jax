d0cccd1d546438d1ddd903e5feeba346e0925155
Jacob Kelly <jacob.jin.kelly@gmail.com>
2020-05-21 13:34:12 -0400

    repro for profiling

--- examples/nodes_ffjord.py
+++ examples/nodes_ffjord.py
@@ -1,11 +1,6 @@
 """
 FFJORD on MNIST, implemented with Haiku.
 """
-import argparse
-import collections
-import os
-import pickle
-import sys
 import time
 
 import haiku as hk
@@ -18,62 +13,25 @@ from jax.flatten_util import ravel_pytree
 from jax.experimental import optimizers
 from jax.experimental.ode import odeint
 from jax.experimental.jet import jet
-from jax.scipy.special import expit as sigmoid
 from jax.scipy.special import logit
 
 from jax.config import config
 config.update("jax_enable_x64", True)
 
-parser = argparse.ArgumentParser('Neural ODE')
-parser.add_argument('--batch_size', type=int, default=200)
-parser.add_argument('--test_batch_size', type=int, default=200)
-parser.add_argument('--nepochs', type=int, default=1)  # TODO: should be 100, just for testing!
-parser.add_argument('--lr', type=float, default=1e-3)
-parser.add_argument('--lam', type=float, default=0)
-parser.add_argument('--lam_w', type=float, default=0)
-parser.add_argument('--atol', type=float, default=1e-6)
-parser.add_argument('--rtol', type=float, default=1e-6)
-parser.add_argument('--method', type=str, default="dopri5")
-parser.add_argument('--no_vmap', action="store_true")
-parser.add_argument('--init_step', type=float, default=1.)
-parser.add_argument('--reg', type=str, choices=['none', 'r2', 'r3', 'r4'], default='none')
-parser.add_argument('--test_freq', type=int, default=300)
-parser.add_argument('--save_freq', type=int, default=300)
-parser.add_argument('--dirname', type=str, default='tmp')
-parser.add_argument('--seed', type=int, default=0)
-parser.add_argument('--no_count_nfe', action="store_true")
-parser.add_argument('--ckpt_freq', type=int, default=300)  # divide test and save, divisible by num_batches
-parser.add_argument('--ckpt_path', type=str, default="./ck.pt")
-parse_args = parser.parse_args()
-
-# TODO: set env vars for memory stuff
-
-
-assert os.path.exists(parse_args.dirname)
-
 # set up config
 
-reg = parse_args.reg
-lam = parse_args.lam
-lam_w = parse_args.lam_w
-seed = parse_args.seed
+reg = "none"
+lam = 0
+lam_w = 0
+seed = 0
+batch_size = 200
+test_batch_size = 200
 rng = jax.random.PRNGKey(seed)
-dirname = parse_args.dirname
-count_nfe = not parse_args.no_count_nfe
-vmap = False if parse_args.no_vmap is True else True
-vmap = False
 num_blocks = 0
 ode_kwargs = {
-    "atol": parse_args.atol,
-    "rtol": parse_args.rtol,
-    # "method": parse_args.method,
-    # "init_step": parse_args.init_step
+    "atol": 1.4e-8,
+    "rtol": 1.4e-8
 }
-
-# jax.nn.softplus jet will fail because of convert element type
-# logaddexp will fail because no lax.max primitive implemented
-# TODO: maybe use jax.nn.softplus when not doing jet?
-# softplus = jax.nn.softplus
 softplus = lambda x: jnp.where(x >= 0,
                                x + jnp.log1p(jnp.exp(-x)),
                                jnp.log1p(jnp.exp(x)))
@@ -153,21 +111,6 @@ class ForwardPreODE(hk.Module):
         return y, logpy
 
 
-class ReversePreODE(hk.Module):
-    """
-    Inverse of module applied before the ODE layer.
-    """
-
-    def __init__(self, alpha=1e-6):
-        super(ReversePreODE, self).__init__()
-        self.alpha = alpha
-
-    def __call__(self, y, logpy):
-        x = (sigmoid(y) - self.alpha) / (1 - 2 * self.alpha)
-        logpx = logpy + logdetgrad(y, self.alpha)
-        return x, logpx
-
-
 class NN_Dynamics(hk.Module):
     """
     NN_Dynamics of the ODENet.
@@ -233,7 +176,7 @@ def initialization_data(input_shape):
     Data for initializing the modules.
     """
     # use the batch size to allocate memory
-    input_shape = (parse_args.test_batch_size, ) + input_shape[1:]
+    input_shape = (test_batch_size, ) + input_shape[1:]
     data = {
         "pre_ode": aug_init(jnp.zeros(input_shape))[:-1],
         "ode": aug_init(jnp.zeros(input_shape))[:-2] + (0., )
@@ -292,15 +235,7 @@ def init_model():
         dy, dp = ffjord_dynamics((y, p), t, eps, params)
         dr = reg_dynamics(y, t, params)
         return dy, dp, dr
-    nodeint_aux = lambda y0, ts, eps, params: odeint(aug_dynamics, y0, ts, eps, params, **ode_kwargs)
     nodeint = lambda y0, ts, eps, params: odeint(aug_dynamics, y0, ts, eps, params, **ode_kwargs)
-
-    def ode_aux(params, y, delta_logp, eps):
-        """
-        Apply the ODE block.
-        """
-        (ys, delta_logps, rs), nfe = nodeint_aux(reg_init(y, delta_logp), ts, eps, params)
-        return (ys[-1], delta_logps[-1], rs[-1]), nfe
 
     def ode(params, y, delta_logp, eps):
         """
@@ -308,17 +243,6 @@ def init_model():
         """
         ys, delta_logps, rs = nodeint(reg_init(y, delta_logp), ts, eps, params)
         return ys[-1], delta_logps[-1], rs[-1]
-
-    def forward_aux(key, params, _images):
-        """
-        Forward pass of the model.
-        """
-        eps = get_epsilon(key, _images.shape)
-
-        z, detla_logp = pre_ode_fn(params["pre_ode"], *aug_init(_images)[:-1])
-        (z, delta_logp, regs), nfe = ode_aux(params["ode"], z, detla_logp, eps)
-
-        return (z, delta_logp, regs), nfe
 
     def forward(key, params, _images):
         """
@@ -337,11 +261,10 @@ def init_model():
     }, "params": {
         "pre_ode": pre_ode_params,
         "ode": dynamics_params
-    },
-        "forward": forward
+    }
     }
 
-    return forward_aux, model
+    return forward, model
 
 
 def aug_init(y):
@@ -399,11 +322,11 @@ def loss_fn(forward, params, images, key):
     """
     The loss function for training.
     """
-    (z, delta_logp, regs), nfe = forward(key, params, images)
+    z, delta_logp, regs = forward(key, params, images)
     loss_ = _loss_fn(z, delta_logp)
     reg_ = _reg_loss_fn(regs)
     weight_ = _weight_fn(params)
-    return loss_ + lam * reg_ + lam_w * weight_, nfe
+    return loss_ + lam * reg_ + lam_w * weight_
 
 
 def init_data():
@@ -415,23 +338,17 @@ def init_data():
                                              shuffle_files=True,
                                              with_info=True,
                                              as_supervised=True,
-                                             read_config=tfds.ReadConfig(shuffle_seed=parse_args.seed,
+                                             read_config=tfds.ReadConfig(shuffle_seed=seed,
                                                                          try_autocache=False))
 
     num_train = ds_info.splits['train'].num_examples
     num_test = ds_info.splits['test'].num_examples
 
-    # make sure all batches are the same size to minimize jit compilation cache
-    assert num_train % parse_args.batch_size == 0
-    num_batches = num_train // parse_args.batch_size
-    assert num_test % parse_args.test_batch_size == 0
-    num_test_batches = num_test // parse_args.test_batch_size
+    num_batches = num_train // batch_size
+    num_test_batches = num_test // test_batch_size
 
-    # make sure we always save the model on the last iteration
-    assert num_batches * parse_args.nepochs % parse_args.save_freq == 0
-
-    ds_train = ds_train.cache().repeat().shuffle(1000, seed=seed).batch(parse_args.batch_size)
-    ds_test_eval = ds_test.batch(parse_args.test_batch_size).repeat()
+    ds_train = ds_train.cache().repeat().shuffle(1000, seed=seed).batch(batch_size)
+    ds_test_eval = ds_test.batch(test_batch_size).repeat()
 
     ds_train, ds_test_eval = tfds.as_numpy(ds_train), tfds.as_numpy(ds_test_eval)
 
@@ -443,36 +360,20 @@ def init_data():
     return ds_train, ds_test_eval, meta
 
 
-def run():
+def run(itrs):
     """
     Run the experiment.
     """
-    print("Reg: %s\tLambda %.4e" % (reg, lam))
-    print("Reg: %s\tLambda %.4e" % (reg, lam), file=sys.stderr)
 
     # init the model first so that jax gets enough GPU memory before TFDS
     forward, model = init_model()
     grad_fn = jax.grad(lambda *args: loss_fn(forward, *args))
 
     ds_train, ds_test_eval, meta = init_data()
-    num_batches = meta["num_batches"]
-    num_test_batches = meta["num_test_batches"]
 
     opt_init, opt_update, get_params = optimizers.adam(step_size=1e-3)
-    unravel_opt = ravel_pytree(opt_init(model["params"]))[1]
-    if os.path.exists(parse_args.ckpt_path):
-        outfile = open(parse_args.ckpt_path, 'rb')
-        state_dict = pickle.load(outfile)
-        outfile.close()
-
-        opt_state = unravel_opt(state_dict["opt_state"])
-
-        load_itr = state_dict["itr"]
-    else:
-        init_params = model["params"]
-        opt_state = opt_init(init_params)
-
-        load_itr = 0
+    init_params = model["params"]
+    opt_state = opt_init(init_params)
 
     @jax.jit
     def update(_itr, _opt_state, _key, _batch):
@@ -481,118 +382,23 @@ def run():
         """
         return opt_update(_itr, grad_fn(get_params(_opt_state), _batch, _key), _opt_state)
 
-    @jax.jit
-    def sep_losses(_opt_state, _batch, _key):
-        """
-        Convenience function for calculating losses separately.
-        """
-        z, delta_logp, regs = model["forward"](_key, get_params(_opt_state), _batch)
-        loss_ = _loss_fn(z, delta_logp)
-        reg_ = _reg_loss_fn(regs)
-        total_loss_ = loss_ + lam * reg_
-        return total_loss_, loss_, reg_
-
-    def evaluate_loss(opt_state, _key, ds_eval):
-        """
-        Convenience function for evaluating loss over train set in smaller batches.
-        """
-        sep_loss_aug_, sep_loss_, sep_loss_reg_, nfe = [], [], [], []
-
-        for test_batch_num in range(num_test_batches):
-            _key, _key2 = jax.random.split(_key, num=2)
-            test_batch = next(ds_eval)[0]
-            test_batch = (test_batch.astype(jnp.float64) + jax.random.uniform(_key2,
-                                                                              minval=1e-15,
-                                                                              maxval=1 - 1e-15,
-                                                                              shape=test_batch.shape)) / 256.
-
-            test_batch_loss_aug_, test_batch_loss_, test_batch_loss_reg_ = sep_losses(opt_state, test_batch, _key)
-
-            nfe.append(0)
-
-            sep_loss_aug_.append(test_batch_loss_aug_)
-            sep_loss_.append(test_batch_loss_)
-            sep_loss_reg_.append(test_batch_loss_reg_)
-
-        sep_loss_aug_ = jnp.array(sep_loss_aug_)
-        sep_loss_ = jnp.array(sep_loss_)
-        sep_loss_reg_ = jnp.array(sep_loss_reg_)
-        nfe = jnp.array(nfe)
-
-        return jnp.mean(sep_loss_aug_), jnp.mean(sep_loss_), jnp.mean(sep_loss_reg_), jnp.mean(nfe)
-
     itr = 0
-    info = collections.defaultdict(dict)
 
     key = rng
 
-    for epoch in range(parse_args.nepochs):
-        for i in range(num_batches):
-            key, key2 = jax.random.split(key, num=2)
-            batch = next(ds_train)[0]
-            batch = (batch.astype(jnp.float64) + jax.random.uniform(key2,
-                                                                    minval=1e-15,
-                                                                    maxval=1 - 1e-15,
-                                                                    shape=batch.shape)) / 256.
+    for i in range(itrs):
+        key, key2 = jax.random.split(key, num=2)
+        batch = next(ds_train)[0]
+        batch = (batch.astype(jnp.float64) + jax.random.uniform(key2,
+                                                                minval=1e-15,
+                                                                maxval=1 - 1e-15,
+                                                                shape=batch.shape)) / 256.
 
-            itr += 1
+        itr += 1
 
-            if itr <= load_itr:
-                continue
-
-            update_start = time.time()
-            opt_state = update(itr, opt_state, key, batch)
-            tree_flatten(opt_state)[0][0].block_until_ready()
-            update_end = time.time()
-            time_str = "%d %.18f %.4f %.4f %d\n" % (itr, update_end - update_start, load_itr)
-            outfile = open("%s/reg_%s_lam_%.18e_num_blocks_%d_time.txt" % (dirname, reg, lam, num_blocks), "a")
-            outfile.write(time_str)
-            outfile.close()
-
-            if itr % parse_args.test_freq == 0:
-                loss_aug_, loss_, loss_reg_, nfe_ = evaluate_loss(opt_state, key, ds_test_eval)
-
-                print_str = 'Iter {:04d} | Total (Regularized) Loss {:.6f} | ' \
-                            'Loss {:.6f} | r {:.6f} | NFE {:.6f}'.format(itr, loss_aug_, loss_, loss_reg_, nfe_)
-
-                print(print_str)
-
-                outfile = open("%s/reg_%s_lam_%.18e_num_blocks_%d_info.txt" % (dirname, reg, lam, num_blocks), "a")
-                outfile.write(print_str + "\n")
-                outfile.close()
-
-                info[itr]["loss_aug"] = loss_aug_
-                info[itr]["loss"] = loss_
-                info[itr]["loss_reg"] = loss_reg_
-                info[itr]["nfe"] = nfe_
-
-            if itr % parse_args.save_freq == 0:
-                param_filename = "%s/reg_%s_lam_%.18e_%d_fargs.pickle" % (dirname, reg, lam, itr)
-                fargs = get_params(opt_state)
-                outfile = open(param_filename, "wb")
-                pickle.dump(fargs, outfile)
-                outfile.close()
-
-            outfile = open("%s/reg_%s_lam_%.18e_num_blocks_%d_iter.txt" % (dirname, reg, lam, num_blocks), "a")
-            outfile.write("Iter: {:04d}\n".format(itr))
-            outfile.close()
-
-            if itr % parse_args.ckpt_freq == 0:
-                state_dict = {
-                    "opt_state": ravel_pytree(opt_state)[0],
-                    "itr": itr,
-                }
-                outfile = open(parse_args.ckpt_path, 'wb')
-                pickle.dump(state_dict, outfile)
-                outfile.close()
-    meta = {
-        "info": info,
-        "args": parse_args
-    }
-    outfile = open("%s/reg_%s_lam_%.18e_num_blocks_%d_meta.pickle" % (dirname, reg, lam, num_blocks), "wb")
-    pickle.dump(meta, outfile)
-    outfile.close()
-
-
-if __name__ == "__main__":
-    run()
+        update_start = time.time()
+        opt_state = update(itr, opt_state, key, batch)
+        tree_flatten(opt_state)[0][0].block_until_ready()
+        update_end = time.time()
+        time_str = "%d %.18f" % (itr, update_end - update_start)
+        print(time_str)
